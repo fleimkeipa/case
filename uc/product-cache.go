8b7d9d33dd4ba555852cd3a5861bd265741156de
fleimkeipa <adem_sahin12@hotmail.com>
2024-11-12 01:41:12 +0300

    Change cache exist control

--- uc/product-cache.go
+++ uc/product-cache.go
@@ -2,38 +2,41 @@ package uc
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/fleimkeipa/case/pkg"
-
-	"github.com/redis/go-redis/v9"
+	"github.com/fleimkeipa/case/repositories/interfaces"
 )
 
 type ProductCacheUC struct {
-	cache redis.Client
+	repo interfaces.CacheRepository
 }
 
-func NewProductCacheUC(cache redis.Client) *ProductCacheUC {
+func NewProductCacheUC(repo interfaces.CacheRepository) *ProductCacheUC {
 	return &ProductCacheUC{
-		cache: cache,
+		repo: repo,
 	}
 }
 
 func (rc *ProductCacheUC) IsExist(ctx context.Context, brandID int, barcode string) bool {
-	cacheID := pkg.ProductCacheID(brandID, barcode)
-	res, err := rc.cache.Get(ctx, cacheID).Result()
+	cacheID := ProductCacheID(brandID, barcode)
+	res, err := rc.repo.Exists(ctx, cacheID)
 	if err != nil {
 		return false
 	}
 
-	if res != "" {
+	if res > 0 {
 		return true
 	}
 
-	go func() {
-		if err := rc.cache.Set(ctx, cacheID, barcode, 0).Err(); err != nil {
+	go func(ctx context.Context, cacheID string, barcode string) {
+		if err := rc.repo.Set(ctx, cacheID, barcode); err != nil {
 			return
 		}
-	}()
+	}(ctx, cacheID, barcode)
 
 	return false
+}
+
+func ProductCacheID(brandID int, barcode string) string {
+	return fmt.Sprintf("product:%d:%v", brandID, barcode)
 }
