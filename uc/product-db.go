e89eb665954e75966fd4fb8cd642c7f729572721
fleimkeipa <adem_sahin12@hotmail.com>
2024-11-11 21:27:08 +0300

    Add cache control on db create func

--- uc/product-db.go
+++ uc/product-db.go
@@ -1,18 +1,28 @@
 package uc
 
 import (
+	"context"
+
 	"github.com/fleimkeipa/case/model"
 	"github.com/fleimkeipa/case/repositories/interfaces"
 )
 
 type ProductDBUC struct {
-	repo interfaces.ProductDBRepository
+	repo  interfaces.ProductDBRepository
+	cache *ProductCacheUC
 }
 
-func NewProductDBUC(repo interfaces.ProductDBRepository) *ProductDBUC {
-	return &ProductDBUC{repo: repo}
+func NewProductDBUC(repo interfaces.ProductDBRepository, cache *ProductCacheUC) *ProductDBUC {
+	return &ProductDBUC{
+		repo:  repo,
+		cache: cache,
+	}
 }
 
-func (rc *ProductDBUC) Create(product *model.Product) (*model.Product, error) {
+func (rc *ProductDBUC) Create(ctx context.Context, product *model.Product) (*model.Product, error) {
+	if rc.cache.IsExist(ctx, product.BrandID, product.Barcode) {
+		return product, nil
+	}
+
 	return rc.repo.Create(product)
 }
