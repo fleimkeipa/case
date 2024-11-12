34bdaa481e5d77cab45f176912ac36be26cfe671
fleimkeipa <adem_sahin12@hotmail.com>
2024-11-12 19:56:28 +0300

    Add product repo api tests

---
+++ tests/repo_product_api_test.go
@@ -0,0 +1,59 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	"github.com/fleimkeipa/case/model"
+	"github.com/fleimkeipa/case/pkg"
+	"github.com/fleimkeipa/case/repositories"
+)
+
+func init() {
+	os.Setenv("API_KEY", "")
+	os.Setenv("API_SECRET", "")
+	os.Setenv("SUPPLIER_ID", "")
+}
+
+func TestProductAPIRepository_FindAll(t *testing.T) {
+	type fields struct {
+		Client pkg.Client
+	}
+	type args struct {
+		suplierID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.ProductsResponse
+		wantErr bool
+	}{
+		{
+			name: "",
+			fields: fields{
+				Client: pkg.NewHTTPClient(os.Getenv("API_KEY"), os.Getenv("API_SECRET")),
+			},
+			args: args{
+				suplierID: os.Getenv("SUPPLIER_ID"),
+			},
+			want:    &model.ProductsResponse{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rc := &repositories.ProductAPIRepository{
+				Client: tt.fields.Client,
+			}
+			got, err := rc.FindAll(tt.args.suplierID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ProductAPIRepository.FindAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got.Content == nil {
+				t.Errorf("ProductAPIRepository.FindAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
